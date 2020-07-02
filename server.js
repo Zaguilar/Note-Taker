f2349d9fb8d6f27f791d329bd311ab726388d32a
Zachary Aguilar <zacharyjag@yahoo.com>
2020-07-02 17:09:55 -0500

    changes for specificity

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const notes = require('./db/db.json');
 const fs = require('fs');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Sets up the Express app to handle data parsing
@@ -11,11 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// ------------------------------------------------------------------------
+// will read the db.json file
 app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
-
+// will save new not on request body, adding it to json and return new note to client
 app.post('/api/notes', (req, res) => {
   req.body.id = notes.length;
   const newNote = req.body;
@@ -32,7 +32,7 @@ app.delete('/api/notes/:id', (req, res) => {
   const deleteIndex = req.params.id;
   notes.splice(deleteIndex, 1);
 
-  //assign noteids
+  //assign noteids for deletion
   for(let i = 0; i < notes.length; i++){
     console.log(notes[i]);
     notes[i].id = i;
@@ -48,10 +48,11 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 
 // routes for html
+// returns notes.html
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
-
+// returns index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
