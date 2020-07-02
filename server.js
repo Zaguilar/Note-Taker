d4f5b9ca2539f8e6a7f58b15d95d6443b840e6f5
Zachary Aguilar <zacharyjag@yahoo.com>
2020-07-02 16:50:58 -0500

    test bench

--- server.js
+++ server.js
@@ -1,3 +1,61 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const express = require('express');
+const path = require('path');
+const notes = require('./db/db.json');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// ------------------------------------------------------------------------
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = notes.length;
+  const newNote = req.body;
+  notes.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, '/db/db.json'),
+    JSON.stringify(notes, null, 2)
+  );
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  //remove the appropriate note
+  const deleteIndex = req.params.id;
+  notes.splice(deleteIndex, 1);
+
+  //assign noteids
+  for(let i = 0; i < notes.length; i++){
+    console.log(notes[i]);
+    notes[i].id = i;
+    console.log(notes[i]);
+  }
+
+  fs.writeFileSync(
+    path.join(__dirname, '/db/db.json'), 
+    JSON.stringify(notes, null, 2)
+  );
+  //update the db.json file
+  res.json(req.body);
+});
+
+// routes for html
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
